ee64ed955470a99037437f8b6852524e4d6d5e42
Adam Shin <adamjshin@gmail.com>
2020-05-01 13:12:02 -0600

    Project cleanup

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -10,26 +10,18 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication,
         didFinishLaunchingWithOptions
         launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = MainViewController()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication,
-        configurationForConnecting connectingSceneSession: UISceneSession,
-        options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        
-        return UISceneConfiguration(
-            name: "Default Configuration",
-            sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication,
-        didDiscardSceneSessions sceneSessions: Set<UISceneSession>) { }
     
 }
